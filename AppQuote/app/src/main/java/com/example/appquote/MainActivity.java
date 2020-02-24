5d77905bd1a8a50cfc7d02572b854e29e3cc33c9
Alberto Berenguer <alberto.berenguer.97@gmail.com>
2020-02-24 13:46:53 +0100

    Que se joda el que merge

--- AppQuote/app/src/main/java/com/example/appquote/MainActivity.java
+++ AppQuote/app/src/main/java/com/example/appquote/MainActivity.java
@@ -5,19 +5,19 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.Manifest;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Matrix;
-import android.graphics.Rect;
+import android.graphics.Paint;
 import android.net.Uri;
 import android.os.Environment;
+import android.os.StrictMode;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -26,38 +26,40 @@ import android.util.Log;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.content.Intent;
-
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.fragment.app.FragmentActivity;
 import android.content.pm.PackageManager;
 import android.content.Context;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.TextView;
-
-import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE;
-
-
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Camera mCamera;
     private CameraPreview mPreview;
-    private TextView quote;
     private ImageView img;
+    private Button share,save;
+    private Canvas canvas;
 
     private int currentCameraId = 1; //camara frontal
+    private Bitmap currentImage;
+    private byte[] dataPicture=null;
     public static final int MEDIA_TYPE_IMAGE = 1;
     public static final int MEDIA_TYPE_VIDEO = 2;
     private static  final int FOCUS_AREA_SIZE= 300;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("¿El dispositivo tiene camara? "+ checkCameraHardware(this));
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
                 != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat
@@ -65,6 +67,9 @@ public class MainActivity extends AppCompatActivity {
                             MainActivity.this,
                             new String[] { Manifest.permission.CAMERA },
                             100);
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
         }
 
         // Create an instance of Camera
@@ -76,12 +81,14 @@ public class MainActivity extends AppCompatActivity {
         preview.addView(mPreview);
 
 
-
     //hacer foto
        final Button close = (Button) findViewById(R.id.button_close);
         Button captureButton = (Button) findViewById(R.id.button_capture);
         img= (ImageView)  findViewById(R.id.imageView);
-
+        share =(Button) findViewById(R.id.share);
+        share.setVisibility(View.GONE);
+        save = (Button) findViewById(R.id.save);
+        save.setVisibility(View.GONE);
 
         captureButton.setOnClickListener(
                 new View.OnClickListener() {
@@ -89,14 +96,10 @@ public class MainActivity extends AppCompatActivity {
                     public void onClick(View v) {
                         // get an image from the camera
                         img.setVisibility(View.VISIBLE);
+                        save.setVisibility(View.VISIBLE);
+                        share.setVisibility(View.VISIBLE);
                         close.setVisibility(View.VISIBLE);
-                        quote = (TextView)  findViewById(R.id.quote);
                         mCamera.takePicture(null, null, mPicture);
-                        if(quote.getVisibility() == View.VISIBLE){ //si es Visible lo pones Gone
-                            quote.setVisibility(View.GONE);
-                        }else{ // si no es Visible, lo pones
-                            quote.setVisibility(View.VISIBLE);
-                        }
 
                     }
                 }
@@ -135,21 +138,78 @@ public class MainActivity extends AppCompatActivity {
                     public void onClick(View v) {
                     img.setVisibility(View.GONE);
                     close.setVisibility(View.GONE);
+                  //  quote.setVisibility(View.GONE);
+                    save.setVisibility(View.GONE);
+                    share.setVisibility(View.GONE);
                     }
 
                 });
+        save.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        savePicture();
+                                    }});
 
-    }
+        decorView.setOnTouchListener(new View.OnTouchListener() {
 
-    /** Check if this device has a camera */
-    private boolean checkCameraHardware(Context context) {
-        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
-            // this device has a camera
-            return true;
-        } else {
-            // no camera on this device
-            return false;
-        }
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                if (event.getAction() == MotionEvent.ACTION_MOVE) {
+
+                    Paint paint = new Paint();
+
+                    canvas = new Canvas(currentImage);
+                    canvas.save();
+                    paint.setColor(Color.WHITE);
+                    paint.setTextSize(32);
+                    paint.setAntiAlias(true);
+                    paint.setSubpixelText(true);
+                    float x = img.getWidth()/canvas.getWidth();
+                    float y = img.getHeight()/canvas.getHeight();
+                    canvas.drawText("Makinote",  event.getX()/(x*2),   event.getY()/(y), paint);
+                    canvas.restore();
+                    img.invalidate();
+                }
+
+                return true;
+            }
+
+        });
+
+        Button share = (Button) findViewById(R.id.share);
+        share.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+                        StrictMode.setVmPolicy(builder.build());
+                        Bitmap bitmap = currentImage;
+                        RelativeLayout layout = new RelativeLayout(MainActivity.this);
+
+
+                File root = Environment.getExternalStorageDirectory();
+                File cachePath = new File(root.getAbsolutePath() + "/image.jpg");
+                try
+                {
+                    cachePath.createNewFile();
+                    FileOutputStream ostream = new FileOutputStream(cachePath);
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, ostream);
+                    ostream.close();
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+
+                Intent share = new Intent(Intent.ACTION_SEND);
+                share.setType("image/*");
+                share.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(new File(root.getAbsolutePath() + "/image.jpg")));
+                startActivity(Intent.createChooser(share,"Share via"));
+
+            }
+        });
     }
 
     public static Camera getCameraInstance(int id){
@@ -177,9 +237,6 @@ public class MainActivity extends AppCompatActivity {
         public CameraPreview(Context context, Camera camera) {
             super(context);
             mCamera = camera;
-
-            // Install a SurfaceHolder.Callback so we get notified when the
-            // underlying surface is created and destroyed.
             mHolder = getHolder();
             mHolder.addCallback(this);
         }
@@ -219,6 +276,12 @@ public class MainActivity extends AppCompatActivity {
 
             // start preview with new settings
             try {
+
+                Camera.Parameters params = mCamera.getParameters();
+                List<Camera.Size> sizes = params.getSupportedPreviewSizes();
+                Camera.Size optimalSize = getOptimalPreviewSize(sizes,w,h);
+                params.setPreviewSize(optimalSize.width,optimalSize.height);
+                mCamera.setParameters(params);
                 mCamera.setPreviewDisplay(mHolder);
                 mCamera.startPreview();
                 mCamera.autoFocus(null);
@@ -237,18 +300,11 @@ public class MainActivity extends AppCompatActivity {
 
     /** Create a File for saving an image or video */
     private static File getOutputMediaFile(int type){
-        // To be safe, you should check that the SDCard is mounted
-        // using Environment.getExternalStorageState() before doing this.
-
-        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES), "MyCameraApp");
-        // This location works best if you want the created images to be shared
-        // between applications and persist after your app has been uninstalled.
-
-        // Create the storage directory if it does not exist
+        String rootPath=Environment.getExternalStorageDirectory() + "/Appquote/pictures";
+        File mediaStorageDir = new File(rootPath);
         if (! mediaStorageDir.exists()){
             if (! mediaStorageDir.mkdirs()){
-                Log.d("MyCameraApp", "failed to create directory");
+                Log.d("AppQuote", "failed to create directory");
                 return null;
             }
         }
@@ -259,9 +315,6 @@ public class MainActivity extends AppCompatActivity {
         if (type == MEDIA_TYPE_IMAGE){
             mediaFile = new File(mediaStorageDir.getPath() + File.separator +
                     "IMG_"+ timeStamp + ".jpg");
-        } else if(type == MEDIA_TYPE_VIDEO) {
-            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
-                    "VID_"+ timeStamp + ".mp4");
         } else {
             return null;
         }
@@ -269,36 +322,29 @@ public class MainActivity extends AppCompatActivity {
         return mediaFile;
     }
 
-
-
     private Camera.PictureCallback mPicture = new Camera.PictureCallback() {
 
         @Override
         public void onPictureTaken(byte[] data, Camera camera) {
-
-
             Bitmap photo = BitmapFactory.decodeByteArray(data , 0, data.length);
-            Matrix matrix = new Matrix();
-            matrix.postRotate(-90);
-            photo = Bitmap.createBitmap(photo, 0, 0, photo.getWidth(), photo.getHeight(), matrix, true);
+            System.out.println(photo.getHeight());
+            if(currentCameraId==1) {
+                Matrix matrix = new Matrix();
+                matrix.postRotate(-90);
+                float cx = photo.getWidth() / 2f;
+                float cy = photo.getHeight() / 2f;
+                photo = Bitmap.createBitmap(photo, 0, 0, photo.getWidth(), photo.getHeight(), matrix, true);
+                matrix = new Matrix();
+                matrix.postScale(-1, 1, cx, cy);
+                photo = Bitmap.createBitmap(photo, 0, 0, photo.getWidth(), photo.getHeight(), matrix, true);
+            }else{
+                Matrix matrix = new Matrix();
+                matrix.postRotate(90);
+                photo = Bitmap.createBitmap(photo, 0, 0, photo.getWidth(), photo.getHeight(), matrix, true);
+            }
+            dataPicture=data;
             img.setImageBitmap(photo);
-
-
-            File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
-            if (pictureFile == null){
-                // Log.d(TAG, "Error creating media file, check storage permissions");
-                return;
-            }
-
-            try {
-                FileOutputStream fos = new FileOutputStream(pictureFile);
-                fos.write(data);
-                fos.close();
-            } catch (FileNotFoundException e) {
-                // Log.d(TAG, "File not found: " + e.getMessage());
-            } catch (IOException e) {
-                // Log.d(TAG, "Error accessing file: " + e.getMessage());
-            }
+            currentImage=photo;
         }
     };
 
@@ -319,12 +365,56 @@ public class MainActivity extends AppCompatActivity {
     public boolean onTouchEvent(MotionEvent event){
         if(event.getAction() == MotionEvent.ACTION_DOWN){
             Log.d("down", "focusing now");
-
             mCamera.autoFocus(null);
         }
-
         return true;
     }
 
+    private Camera.Size getOptimalPreviewSize(List<Camera.Size> sizes, int w, int h) {
+        final double ASPECT_TOLERANCE = 0.1;
+        double targetRatio=(double)h / w;
+        if (sizes == null) return null;
+        Camera.Size optimalSize = null;
+        double minDiff = Double.MAX_VALUE;
 
+        int targetHeight = h;
+
+        for (Camera.Size size : sizes) {
+            double ratio = (double) size.width / size.height;
+            if (Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE) continue;
+            if (Math.abs(size.height - targetHeight) < minDiff) {
+                optimalSize = size;
+                minDiff = Math.abs(size.height - targetHeight);
+            }
+        }
+
+        if (optimalSize == null) {
+            minDiff = Double.MAX_VALUE;
+            for (Camera.Size size : sizes) {
+                if (Math.abs(size.height - targetHeight) < minDiff) {
+                    optimalSize = size;
+                    minDiff = Math.abs(size.height - targetHeight);
+                }
+            }
+        }
+        return optimalSize;
+    }
+
+    public void savePicture(){
+          File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
+            if (pictureFile == null){
+                return;
+            }
+            try {
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                Bitmap bitmap = currentImage;
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+                fos.close();
+                Toast.makeText(getApplicationContext(),"Imagen guardada",Toast.LENGTH_SHORT).show();
+            } catch (FileNotFoundException e) {
+                Toast.makeText(getApplicationContext(),"Se ha producido un error",Toast.LENGTH_SHORT).show();
+            } catch (IOException e) {
+                Toast.makeText(getApplicationContext(),"Se ha producido un error",Toast.LENGTH_SHORT).show();
+            }
+    }
 }
